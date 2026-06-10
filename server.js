26469a92ce706cafda264d4bada7750da3ce1ed9
jengasite-kenya <aliomarion001@gmail.com>
2026-06-10 09:26:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -862,14 +862,12 @@ function replacePlaceholders(template, customer) {
     if (customer.googleAnalyticsId && customer.googleAnalyticsId !== 'G-XXXXXXXXXX') {
         const gaScript = `<script async src="https://www.googletagmanager.com/gtag/js?id=${customer.googleAnalyticsId}"></script>
 <script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${customer.googleAnalyticsId}');</script>`;
-        html = html.replace('</head>', gaScript + '
-</head>');
+        html = html.replace('</head>', gaScript + '\n</head>');
     }
 
     if (customer.tiktokPixelId && customer.tiktokPixelId !== 'G-XXXXXXXXXX') {
         const ttScript = `<script>!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};ttq.load('${customer.tiktokPixelId}');ttq.page();}(window,document,'ttq');</script>`;
-        html = html.replace('</head>', ttScript + '
-</head>');
+        html = html.replace('</head>', ttScript + '\n</head>');
     }
 
     // Final sweep — blank any remaining unreplaced {{...}} placeholders.
@@ -960,8 +958,7 @@ function replacePlaceholders(template, customer) {
 
     if (schemas.length > 0) {
         const ldScript = `<script type="application/ld+json">${JSON.stringify(schemas.length === 1 ? schemas[0] : schemas)}</script>`;
-        html = html.replace('</head>', ldScript + '
-</head>');
+        html = html.replace('</head>', ldScript + '\n</head>');
     }
 
     return html;
